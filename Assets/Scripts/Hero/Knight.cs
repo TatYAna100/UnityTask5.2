8e641c511cf6e0d07cb0aa9fe027e2ad06cd7fee
TatYAna100 <102324751+TatYAna100@users.noreply.github.com>
2022-09-16 19:32:36 +0300

    Update Knight.cs

--- Assets/Scripts/Hero/Knight.cs
+++ Assets/Scripts/Hero/Knight.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Knight : MonoBehaviour
 {
-    public int Ñoins;
+    public int Coins;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Coin coin))
         {
-            Ñoins++;
+            Coins++;
         }
     }
 }
